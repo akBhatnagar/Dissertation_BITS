6c722829b470ea6063d317c304c49245e19184b8
Akshay Bhatnagar <akshaybhatnagar1998@gmail.com>
2024-01-14 23:12:02 +0530

    Project Initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <Menu/>
+        <br/>
+        <Title/>
+      </h1>
     </div>
+  );
+}
+
+function Title() {
+  return (
+    <div>
+      Integrated Personal Expense Management System with Collaborative Expenditure Allocation for Social Engagements
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+      <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
   );
 }
 
