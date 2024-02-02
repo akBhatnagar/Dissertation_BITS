74025cf3e5c7cd706d818212e38ee060508557e6
Akshay Bhatnagar <akshaybhatnagar1998@gmail.com>
2024-02-03 01:39:56 +0530

    Added login, tailwind and template for other pages

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import './App.css';
-import facebook from './facebook-icon.png';
-import twitter from './twitter-icon.png';
-import gmail from './gmail-icon.png';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import HeaderMenu from './components/HeaderMenu';
+import FooterMenu from './components/FooterMenu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import bgImage from './assets/bg-image.jpg'
 
 function App() {
   return (
-    <div>
-      <h1>
-        <Menu/>
-        <br/>
-        <Title/>
-        <Footer/>
-      </h1>
-    </div>
-  );
-}
-
-function Title() {
-  return (
-    <div>
-      Integrated Personal Expense Management System with Collaborative Expenditure Allocation for Social Engagements
-    </div>
-  );
-}
-
-function Menu() {
-  return (
-      <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+      <div className='App'>
+        <img className='absolute w-full h-full object-cover mix-blend-overlay bg-center' src={bgImage} alt="/" />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Login />} />
+            <Route exact path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+        <FooterMenu />
       </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div>
-      <footer>
-        <div class="footer-content">
-          <div class="social-icons">
-            <a href="https://www.facebook.com/aksh1902/"><img src={facebook} alt="Facebook"></img></a>
-            <a href="https://twitter.com/coder_akshay"><img src={twitter} alt="Twitter"></img></a>
-            <a href="mailto:akshaybhatnagar1998@gmail.com"><img src={gmail} alt="Email"></img></a>
-          </div>
-        </div>
-      </footer>
-    </div>
   );
 }
 
