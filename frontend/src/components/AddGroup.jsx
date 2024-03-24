b8e98b0e57a5e9f3b22c60d8b5cd6db6b1994c46
Akshay Bhatnagar <akshaybhatnagar1998@gmail.com>
2024-03-25 01:10:08 +0530

    Removed unused imports

--- frontend/src/components/AddGroup.jsx
+++ frontend/src/components/AddGroup.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import InformationBox from './InformationBox';
 
 const AddGroup = ({ userId, onAddGroup }) => {
     const [groupName, setGroupName] = useState('');
